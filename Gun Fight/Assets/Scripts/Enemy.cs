935c1ecdf2a79f7ae06902c78265af85ff6c045d
Ilgın <52226386+ilginavci@users.noreply.github.com>
2020-10-22 18:14:32 +0300

    bot düzenlemesi

--- Gun Fight/Assets/Scripts/Enemy.cs
+++ Gun Fight/Assets/Scripts/Enemy.cs
@@ -8,13 +8,20 @@ public class Enemy : MonoBehaviour
     public float enemySpeed;
     public bool directionUp;
     public float maxposY, minposY;
-    
+    public bool random;
+    public GameObject enemyBullet;
+    public float cooldown, cooldownMax;
+    public Transform spawnPoint;
 
-
-    void Update()
-    {  if (directionUp)
+    void Start()
+    { //Random Movement
+        InvokeRepeating("RandomMovement", 0, 5);
+    }
+    void FixedUpdate()
+    {   //Enemy Movement
+        if (directionUp)
         { 
-            transform.Translate(Vector2.up * enemySpeed * Time.deltaTime);
+            transform.Translate(Vector2.up * enemySpeed * Time.fixedDeltaTime);
             if(transform.position.y > maxposY)
             {
                 directionUp = !directionUp;
@@ -22,14 +29,38 @@ public class Enemy : MonoBehaviour
         }
     else
         {
-            transform.Translate(Vector2.down * enemySpeed * Time.deltaTime);
+            transform.Translate(Vector2.down * enemySpeed * Time.fixedDeltaTime);
             if (transform.position.y < minposY)
             {
                 directionUp = !directionUp;
             }
+        } 
+        // Bullet Fire and timing
+        if (cooldown <= 0)
+        {
+           cooldown = cooldownMax;
+          Instantiate(enemyBullet, spawnPoint.transform.position, Quaternion.identity);
+
+        }
+        else
+        {
+          cooldown -= Time.deltaTime;
         }
     }
-
+    void RandomMovement()
+    {
+        var randomNumber = (Random.value > 0.5f); // it gives a bool , 1 or 0
+        if (randomNumber)
+        {
+            directionUp = false; 
+            random = true;
+        }
+        else
+        {
+            directionUp = true;
+            random = true;
+        }
+    }
 
 
 }
