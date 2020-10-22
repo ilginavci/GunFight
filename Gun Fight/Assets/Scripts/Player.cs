c0fba79cc26e1d0dc9fa49a32fa8d8d1f20ab7d6
Ilgın <52226386+ilginavci@users.noreply.github.com>
2020-10-22 13:35:58 +0300

    Silah bugu

--- Gun Fight/Assets/Scripts/Player.cs
+++ Gun Fight/Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
 
     private float startPosX, startPosY;
     public float objectPosX,maxposY,minposY;
-    private bool beingHeld = false,canShoot;
+    private bool beingHeld = false;
     public float cooldown,cooldownMax;
     public GameObject bullet;
     public Transform spawnPoint;
@@ -24,27 +24,20 @@ public class Player : MonoBehaviour
                 mousepos = Input.mousePosition;
                 mousepos = Camera.main.ScreenToWorldPoint(mousepos);
 
-            this.gameObject.transform.localPosition = new Vector3(objectPosX, mousepos.y, 0);
-
+            if (mousepos.y < maxposY && mousepos.y > minposY)
+            {
+                this.gameObject.transform.localPosition = new Vector3(objectPosX, mousepos.y, 0);
+            }
             if (cooldown <= 0)
             {
                 cooldown = cooldownMax;
                 Instantiate(bullet,spawnPoint.transform.position,Quaternion.identity);
-                canShoot = false;
+
             }
             else
             {
                 cooldown -= Time.deltaTime;
             }
-        }
-
-        if (gameObject.transform.position.y >= maxposY)
-        {
-            gameObject.transform.position = new Vector2(objectPosX, maxposY - 0.1f);
-        }
-        else if (gameObject.transform.position.y <= minposY)
-        {
-            gameObject.transform.position = new Vector2(objectPosX, minposY + 0.1f);
         }
     }
 
@@ -56,9 +49,8 @@ public class Player : MonoBehaviour
             mousepos = Input.mousePosition;
             mousepos = Camera.main.ScreenToWorldPoint(mousepos);
 
+                startPosY = mousepos.y - this.transform.localPosition.y;
             
-            startPosY = mousepos.y - this.transform.localPosition.y;
-
             beingHeld = true;
         }
 
