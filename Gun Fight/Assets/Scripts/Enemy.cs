9e605cef4673aba5a2920fc754da0b91e4ca7a9e
Ilgın <52226386+ilginavci@users.noreply.github.com>
2020-10-22 14:11:32 +0300

    Bot eklendi

---
+++ Gun Fight/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float enemySpeed;
+    public bool directionUp;
+    public float maxposY, minposY;
+    
+
+
+    void Update()
+    {  if (directionUp)
+        { 
+            transform.Translate(Vector2.up * enemySpeed * Time.deltaTime);
+            if(transform.position.y > maxposY)
+            {
+                directionUp = !directionUp;
+            }
+        }
+    else
+        {
+            transform.Translate(Vector2.down * enemySpeed * Time.deltaTime);
+            if (transform.position.y < minposY)
+            {
+                directionUp = !directionUp;
+            }
+        }
+    }
+
+
+
+}
