b5a1329464c1e550d913428eed5b93d038319b06
BiaXThePanda <huntermig2002@gmail.com>
2020-10-22 12:23:21 +0300

    Player , player kontrol etc.

---
+++ Gun Fight/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private float startPosX, startPosY;
+    public float objectPosX,maxposY,minposY;
+    private bool beingHeld = false;
+    
+
+
+
+
+
+
+    void Update()
+    {
+        if(beingHeld == true)
+        {
+            
+                Vector3 mousepos;
+                mousepos = Input.mousePosition;
+                mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+
+
+
+                this.gameObject.transform.localPosition = new Vector3(objectPosX, mousepos.y, 0);
+            
+            
+        }    
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousepos;
+            mousepos = Input.mousePosition;
+            mousepos = Camera.main.ScreenToWorldPoint(mousepos);
+
+            
+            startPosY = mousepos.y - this.transform.localPosition.y;
+
+            beingHeld = true;
+        }
+
+        
+    }
+
+    private void OnMouseUp()
+    {
+        beingHeld = false;
+    }
+
+
+}
