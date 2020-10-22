93f71f6821c9943c382d7c42ebca8755e7d58dd7
Ilgın <52226386+ilginavci@users.noreply.github.com>
2020-10-22 12:56:30 +0300

    Prefablar yüklendi

---
+++ Gun Fight/Assets/Scripts/BulletManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    private Rigidbody2D bulletRB;
+    public float power;
+    void Start()
+    {   
+        bulletRB = this.GetComponent<Rigidbody2D>();
+          // Player bullet goes right, Enemy bullet goes left
+        if (gameObject.CompareTag("PlayerBullet"))
+        {
+            bulletRB.AddForce(Vector2.right * power);
+        }
+        else
+        {
+            bulletRB.AddForce(Vector2.left * power);
+        }
+            Destroy(gameObject, 3); 
+        
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+    }
+   
+    
+}
