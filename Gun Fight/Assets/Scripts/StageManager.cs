0834c5fd5f3ef85a386baba27da547f23966df3f
Ilgın <52226386+ilginavci@users.noreply.github.com>
2020-10-25 20:08:53 +0300

    enemyguns

---
+++ Gun Fight/Assets/Scripts/StageManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    GameObject[] guns;
+    public Transform enemySpawnPoint;
+    public GameObject gun1, gun2, gun3, gun4, gun5, gun6, gun7, gun8, gun9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        guns = new GameObject[] { gun1, gun2, gun3, gun4, gun5, gun6, gun7, gun8, gun9 };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void RandomGun ()
+    {
+        int randomNumber = Random.Range(0, 2);
+    }
+}
